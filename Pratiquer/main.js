f4eb7f772d90ca4d93d2ae6212c3dec638173d07
camillepellerin-creator <76695622+camillepellerin-creator@users.noreply.github.com>
2021-06-21 14:49:00 +0200

    exo1

---
+++ Pratiquer/main.js
@@ -0,0 +1,23 @@
+function GiveVillainName (){
+    let usersBirthdayMonth = prompt("What's your birthday month?");
+    if (usersBirthdayMonth==="January"){
+        alert("The Evil!😈");
+    }
+    if (usersBirthdayMonth==="February"){
+        alert("The Vile!👹");
+    }
+    if (usersBirthdayMonth==="March"){
+        alert("The Cruel!💀");
+    }
+    if (usersBirthdayMonth==="April"){
+        alert("The Trashy!💩");
+    }
+    if (usersBirthdayMonth==="May"){
+        alert("The Despicable!😡");
+    }
+    if (usersBirthdayMonth==="June"){
+        alert("The Embarrassing!🤮");
+    }
+}
+GiveVillainName();
+   
